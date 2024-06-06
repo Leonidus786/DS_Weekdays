8d21d079f93fa52e84a5814ac8e10dcba6a6e635
Leonidus786 <abhi.upadh@gmail.com>
2024-06-06 17:15:26 +0530

    print, input, str, id(), len()

---
+++ Python/Day_1/String_Manipulation_and_code_Intelligence/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,34 @@
+# print("Hello World!")
+# print("Hello World!")
+# print("Hello World!")
+
+# Instead of writing this 3 times we can use "\n" which is a new line character for doing the same
+# thing in a single line of code
+
+# print("Hello World!\nHello World!\nHello World!")
+
+
+# String --> Set of characters is called strings.
+# Amit = 2
+# print(type(Amit))
+# name = str('Amit')   #using str method
+# print(type(name))
+
+
+# print("Hello" + "Abhishek")  # There should be a space in between two words.
+
+#This would do the same with a space between two words.
+# print("Hello " + "Abhishek") #Space in hello
+# print("Hello" + " Abhishek") # Space before name
+# print("Hello" + " " +"Abhishek") # Space in between two words
+
+# Spaces in python are quite important 
+    # print("Hello World") #this would give a error
+
+
+# input() will get user input in console
+# Then print() will print the word "Hello" and the user input
+# print("Hello " + input("What is your name?"))
+
+
+# print("hello " +input("what is your mail id? "))
---
+++ Python/Day_1/Variables/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,10 @@
+name = "Leonidus"
+print(name)
+
+name = "Abhishek"
+print(name)
+
+
+name = input("What is your name?")
+length = len(name)
+print(length)
